17347392d7a9ee4b0be95c2f7cb03903a7ff6595
Sharath P <Sharath P@LAPTOP-EG336HHP>
2024-10-13 10:54:21 +0530

    POM class for Home page

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+public class HomePage {
+
+	@FindBy(xpath="//a[text()='Logout']")
+	private WebElement logoutLink;
+	
+	
+	public HomePage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public boolean verifyHomePageIsDisplayed(WebDriverWait wait)
+	{
+		try 
+		{
+			wait.until(ExpectedConditions.visibilityOf(logoutLink));
+			Reporter.log("Homepage is Dislpayed",true);
+			return true;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			Reporter.log("Homepage is NotDislpayed",true);
+			return false;
+		}
+	}
+}
